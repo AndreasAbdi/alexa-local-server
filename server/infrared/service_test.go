8795e17d9a418f2c2ed2963a6710c9fc2e619030
Andreas.Abdi <andreasabdi@yahoo.ca>
2018-10-22 16:40:15 -0700

    modified the service_test to use http_test

--- server/infrared/service_test.go
+++ server/infrared/service_test.go
@@ -1,14 +1,21 @@
 package infrared
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/AndreasAbdi/alexa-local-server/server/config"
 )
 
 func TestServiceTurnOnTv(t *testing.T) {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//do nothing. IR blaster endpoint doesn't return anything.
+	})
+	server := httptest.NewServer(handler)
+	defer server.Close()
 	conf := config.Wrapper{
-		IRBlasterAddress:  "http://sometestaddress.com",
+		IRBlasterAddress:  server.URL,
 		IRBlasterPassword: "somefakepassword",
 	}
 	service := NewService(conf)
@@ -16,8 +23,14 @@ func TestServiceTurnOnTv(t *testing.T) {
 }
 
 func TestServiceTurnOnSoundBar(t *testing.T) {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//do nothing. IR blaster endpoint doesn't return anything.
+	})
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
 	conf := config.Wrapper{
-		IRBlasterAddress:  "http://sometestaddress.com",
+		IRBlasterAddress:  server.URL,
 		IRBlasterPassword: "somefakepassword",
 	}
 	service := NewService(conf)
