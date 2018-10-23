6d4aef0cba83275a83e3f812601ba9c7f7a1b331
Andreas.Abdi <andreasabdi@yahoo.ca>
2018-10-22 18:02:16 -0700

    Moved the template configs to a different directory and modified scripts to reflect

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 ENDPOINT=${1:-8000}
 
+PROJ_DIR=$(pwd)
+CONFIG_DIR=${PROJ_DIR}/configs
+TEMPLATE_DIR=${CONFIG_DIR}/server_skill_template
+SKILL_DIR=${CONFIG_DIR}/server_skill
+
 deploy_server() {
     bash -c "exec -a localserver make > /dev/null &"
 }
 
 get_skill_id() {
-    local skill_id=$(grep -hr --exclude-dir=server_skill_template --exclude-dir=.git --exclude-dir=vendor "\"skill_id\"\:" | sed "s|\"skill_id\"\: \"\(.*\)\"\,|\1|g")
+    cd ${SKILL_DIR}
+    local skill_id=$(grep -hr "\"skill_id\"\:" | sed "s|\"skill_id\"\: \"\(.*\)\"\,|\1|g")
+    cd ${PROJ_DIR}
     echo "${skill_id}"
 }
 
 modify_server_config() {
     local skill_id=$1 
-    sed -i "s|\"alexaAppID\"\: \".*\"|\"alexaAppID\"\: \"$1\"|g" ./.serverconf.json
+    sed -i "s|\"alexaAppID\"\: \".*\"|\"alexaAppID\"\: \"$1\"|g" ${PROJ_DIR}/.serverconf.json
 }
 
 build_server_config() {
-    cp ./.serverconf.json.template ./.serverconf.json
+    cp ${PROJ_DIR}/.serverconf.json.template ${PROJ_DIR}/.serverconf.json
 }
 
 deploy_ngrok() {
@@ -29,36 +36,45 @@ get_ngrok_url() {
 }
 
 set_ngrok_address_to_skill() {
-    grep -rl --exclude-dir=server_skill_template --exclude-dir=.git --exclude-dir=vendor "\"uri\"\:" | xargs sed -i "s|\"uri\"\: \".*\"|\"uri\"\: \"https:\/\/$1\/alexa\"|g"
+    cd ${SKILL_DIR}
+    grep -rl "\"uri\"\:" | xargs sed -i "s|\"uri\"\: \".*\"|\"uri\"\: \"https:\/\/$1\/alexa\"|g"
+    cd ${PROJ_DIR}
 }
 
 build_skill() {
-    if [[ ! -d "./server_skill" ]]; then
+    if [[ ! -d "${SKILL_DIR}" ]]; then
         echo "no skill, creating."
+        cd ${CONFIG_DIR}
         ask new -n "server_skill"
-        rm -rf ./server_skill/lambda
-        cp -a ./server_skill_template/. ./server_skill/
+        rm -rf ${SKILL_DIR}/lambda
+        cd ${PROJ_DIR}
     fi
+    cp -a ${TEMPLATE_DIR}/. ${SKILL_DIR}/
+
 }
 
 deploy_skill() {
-    cd ./server_skill
+    echo "deploying skill"
+    cd ${SKILL_DIR}
     ask deploy
-    cd ../
-    pwd
+    cd ${PROJ_DIR}
 }
 
-echo $(pwd)
-echo "Deploying the server"
-deploy_ngrok
-sleep 2s
-address=$(get_ngrok_url)
-echo "Ngrok address is ${address}"
-build_skill
-set_ngrok_address_to_skill ${address}
-deploy_skill
-skill_id=$(get_skill_id)
-echo "skill id is ${skill_id}"
-build_server_config
-modify_server_config ${skill_id}
-deploy_server
+deploy_flow() {
+    echo "Deploying the server"
+    deploy_ngrok
+    sleep 2s
+    address=$(get_ngrok_url)
+    echo "Ngrok address is ${address}"
+    build_skill
+    set_ngrok_address_to_skill ${address}
+    deploy_skill
+    skill_id=$(get_skill_id)
+    echo "skill id is ${skill_id}"
+    build_server_config
+    modify_server_config ${skill_id}
+    deploy_server
+}
+
+
+deploy_flow
--- scripts/kill.sh
+++ scripts/kill.sh
@@ -3,3 +3,5 @@
 ps aux | grep localserver | awk '{print $2}' | xargs kill
 
 ps aux | grep ngrok | awk '{print $2}' | xargs kill
+
+ps aux | grep alexa_local_server | awk '{print $2}' | xargs kill
